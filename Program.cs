629fb221804f036af51356da08add1665fa09dbe
irl <irl@DESKTOP-DIAN65C>
2024-10-03 11:23:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.Write("Bir sayı giriniz: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        if (sayi == 10)
+        {
+            Console.WriteLine("Girilen sayı 10'a eşittir.");
+        }
+        else if (sayi < 10)
+        {
+            Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+        }
+
+        if (sayi % 2 == 0)
+        {
+            Console.WriteLine("Girilen sayı çifttir.");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı tektir.");
+        }
+    }
+}
